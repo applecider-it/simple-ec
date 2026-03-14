6a0944df68ab0b7c78ab400758776e8d21e3050f
owner <owner@localhost>
2026-03-15 07:34:07 +0900

    カートのコードを分離

--- src/app/services/cart_services/cart_service.rb
+++ src/app/services/cart_services/cart_service.rb
@@ -1,18 +1,7 @@
 # カート管理
-# 
-# カートセッション内容
-# 
-# クッキーセッションではJSONになるので、キーは文字列
-# 
-# {
-#   "product.id": {
-#     "amount": int
-#   }
-# }
-# 
-# 例: { "1" => { "amount" => 2 }, "3" => { "amount" => 1 } }
 class CartServices::CartService
   def initialize(session)
+    @data_service = DataService.new(session)
     @session = session
     @session_key = :cart
   end
@@ -23,12 +12,12 @@ class CartServices::CartService
 
     #@session.delete(@session_key)  # 動作確認用
 
-    cart = get_session
+    cart = @data_service.get_session
 
-    init_cart_product(cart, product)
-    add_cart_product(cart, product, add_cart_form)
+    @data_service.init_cart_product(cart, product)
+    @data_service.add_cart_product(cart, product, add_cart_form)
 
-    set_session(cart)
+    @data_service.set_session(cart)
 
     Rails.logger.debug "SESSION :cart"
     p @session[@session_key]
@@ -36,7 +25,7 @@ class CartServices::CartService
 
   # 商品サマリー
   def summary
-    list = cart_list
+    list = @data_service.cart_list
 
     summary_service = OrderServices::SummaryService.new
 
@@ -51,66 +40,14 @@ class CartServices::CartService
   # 商品削除
   def destroy(product)
 
-    cart = get_session
+    cart = @data_service.get_session
 
-    init_cart_product(cart, product)
-    delete_cart_product(cart, product)
+    @data_service.init_cart_product(cart, product)
+    @data_service.delete_cart_product(cart, product)
 
-    set_session(cart)
+    @data_service.set_session(cart)
 
     Rails.logger.debug "SESSION :cart"
     p @session[@session_key]
-  end
-
-  # カート情報
-  private def cart_list
-    cart = get_session
-
-    product_ids = cart.keys
-
-    # index_by(&:id) で { 1 => product_obj, 3 => product_obj } の形にする
-    products = Product.where(id: product_ids).index_by(&:id)
-
-    list = []
-
-    cart.each do |product_id, row|
-      product = products[product_id.to_i]  # セッションキーが文字列のため to_i
-      next unless product  # 商品が削除されている場合はスキップ
-
-      list.push({
-        amount: row["amount"].to_i,
-        product: product
-      })
-    end
-
-    list
-  end
-
-  # 一時データに、商品追加
-  private def add_cart_product(cart, product, add_cart_form)
-    cart[product.id.to_s]["amount"] += add_cart_form.amount.to_i
-  end
-
-  # 一時データの、商品初期化
-  private def init_cart_product(cart, product)
-    initial_data = {
-      "amount" => 0,
-    }
-    cart[product.id.to_s] = cart[product.id.to_s] || initial_data
-  end
-
-  # 一時データの、商品削除
-  private def delete_cart_product(cart, product)
-    cart.delete(product.id.to_s)
-  end
-
-  # セッションから取得
-  private def get_session
-    @session[@session_key] || {}
-  end
-
-  # セッションに設定
-  private def set_session(cart)
-    @session[@session_key] = cart
   end
 end
---
+++ src/app/services/cart_services/cart_service/data_service.rb
@@ -0,0 +1,71 @@
+# カート管理
+# 
+# カートセッション内容
+# 
+# クッキーセッションではJSONになるので、キーは文字列
+# 
+# {
+#   "product.id": {
+#     "amount": int
+#   }
+# }
+# 
+# 例: { "1" => { "amount" => 2 }, "3" => { "amount" => 1 } }
+class CartServices::CartService::DataService
+  def initialize(session)
+    @session = session
+    @session_key = :cart
+  end
+
+  # カート情報
+  def cart_list
+    cart = get_session
+
+    product_ids = cart.keys
+
+    # index_by(&:id) で { 1 => product_obj, 3 => product_obj } の形にする
+    products = Product.where(id: product_ids).index_by(&:id)
+
+    list = []
+
+    cart.each do |product_id, row|
+      product = products[product_id.to_i]  # セッションキーが文字列のため to_i
+      next unless product  # 商品が削除されている場合はスキップ
+
+      list.push({
+        amount: row["amount"].to_i,
+        product: product
+      })
+    end
+
+    list
+  end
+
+  # 一時データに、商品追加
+  def add_cart_product(cart, product, add_cart_form)
+    cart[product.id.to_s]["amount"] += add_cart_form.amount.to_i
+  end
+
+  # 一時データの、商品初期化
+  def init_cart_product(cart, product)
+    initial_data = {
+      "amount" => 0,
+    }
+    cart[product.id.to_s] = cart[product.id.to_s] || initial_data
+  end
+
+  # 一時データの、商品削除
+  def delete_cart_product(cart, product)
+    cart.delete(product.id.to_s)
+  end
+
+  # セッションから取得
+  def get_session
+    @session[@session_key] || {}
+  end
+
+  # セッションに設定
+  def set_session(cart)
+    @session[@session_key] = cart
+  end
+end
