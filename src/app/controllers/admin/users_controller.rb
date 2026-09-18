5b6a4fdef8fc536ff6ea4dc4eaeb52883cb5c992
owner <owner@localhost>
2026-09-18 09:22:36 +0900

    ユーザーにname追加

--- src/app/controllers/admin/users_controller.rb
+++ src/app/controllers/admin/users_controller.rb
@@ -83,7 +83,7 @@ class Admin::UsersController < Admin::BaseController
 
   # 変更可能な項目だけを絞り込む
   private def user_params
-    params.expect(user: [ :email, :password, :password_confirmation ])
+    params.expect(user: [ :email, :name, :password, :password_confirmation ])
   end
 
   # 更新画面の共通処理
