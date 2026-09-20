75fc6a1aa0828e7e0fb25afecaa7863688c948c6
owner <owner@localhost>
2026-09-20 18:09:18 +0900

    購入エラーチェック

--- src/app/models/user_order.rb
+++ src/app/models/user_order.rb
@@ -20,7 +20,8 @@ class UserOrder < ApplicationRecord
   }
 
   validates :payment_method, presence: { message: "%{attribute}を選択してください" }
-  validates :user_address_id, presence: { message: "%{attribute}を選択してください" }
+  validates :user_address, presence: { message: "%{attribute}を選択してください" }
+  validate :user_address_belongs_to_user
 
   # 有効なオーダーステータスのキー文字列と表示名のハッシュ
   def self.active_order_statuses
@@ -30,5 +31,14 @@ class UserOrder < ApplicationRecord
   # 有効な支払方法のキー文字列と表示名のハッシュ
   def self.active_payment_methods
     payment_methods_i18n.select { |key, _| %w[bank_transfer convenience_store cod].include?(key) }
+  end
+
+  # user_address が自分自身(user)の住所であることを検証する
+  private def user_address_belongs_to_user
+    return if user_address.nil? || user.nil?
+
+    if user_address.user_id != user_id
+      errors.add(:user_address_id, "この住所は選択できません")
+    end
   end
 end
